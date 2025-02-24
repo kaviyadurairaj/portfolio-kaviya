063fd6c6529fa88539869aaa7c36eaffc239c11c
kaviyadurairaj <kaviyadurairaj08@gmail.com>
2025-02-24 19:02:26 +0530

    chnages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1 className="logo">My Portfolio</h1>
+      <h1 className="logo">KAVIYA D</h1>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/education">Education</Link></li>
